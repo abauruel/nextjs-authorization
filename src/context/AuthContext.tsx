eee28a37696892dab155f28b2c4735a6291b3cd9
abauruel <abauruel@gmail.com>
2021-06-19 07:33:53 -0300

    add refresh token strategy

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 import { api } from '../services/api'
 import Router from 'next/router'
-import { setCookie } from 'nookies'
+import { setCookie, parseCookies } from 'nookies'
 
 type SignInCredential = {
   email: string,
@@ -29,6 +29,14 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
 
+
+  useEffect(() => {
+    api.get('/me').then(response => {
+      const { email, permissions, roles } = response.data
+      setUser({ email, permissions, roles })
+    })
+  }, [])
+
   async function signIn({ email, password }: SignInCredential) {
 
     try {
@@ -37,10 +45,6 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
       const { token, refreshToken, permissions, roles } = response.data
 
-      setUser({
-        email,
-        permissions, roles
-      })
       setCookie(undefined, '@auth-next.token', token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
@@ -49,6 +53,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
       })
+
+      setUser({
+        email,
+        permissions, roles
+      })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
-export function Dashboard() {
+export default function Dashboard() {
   const { user } = useContext(AuthContext)
   return (
     <h1>Dashboard : {user?.email}</h1>
